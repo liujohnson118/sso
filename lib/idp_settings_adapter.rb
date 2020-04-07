a5252a5dace2a8055c2e6e6da9876e0dbee52706
liujohnson118 <j.liu@ivrnet.com>
2020-04-06 20:23:17 -0600

    add okta settings

--- lib/idp_settings_adapter.rb
+++ lib/idp_settings_adapter.rb
@@ -3,6 +3,8 @@ class IdpSettingsAdapter
     case idp_entity_id
     when Settings.identity_providers.onelogin.idp_entity_id
       return OneLogin::RubySaml::Settings.new(Settings.identity_providers.onelogin)
+    when Settings.identity_providers.okta.idp_entity_id
+      return OneLogin::RubySaml::Settings.new(Settings.identity_providers.okta)
     else
       return {}
     end
