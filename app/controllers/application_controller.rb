5e7c7e31ae19747e5b366b9d61b74bf477fad894
liujohnson118 <j.liu@ivrnet.com>
2020-04-10 17:02:18 -0600

    beautify login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :active_user_session?
+  helper_method :saml_session?
+
+  def active_user_session?
+    @active_user_session ||= (current_user || saml_session?)
+  end
+
+  def saml_session?
+    @saml_session_check ||= session[:nameid] && session[:saml_issuer]
+  end
 
 end
