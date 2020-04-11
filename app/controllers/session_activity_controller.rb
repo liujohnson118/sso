151f3909b8091ab2bc51573e8f71538ed6a65fc5
liujohnson118 <j.liu@ivrnet.com>
2020-04-11 13:35:43 -0600

    cleanup

--- app/controllers/session_activity_controller.rb
+++ app/controllers/session_activity_controller.rb
@@ -4,7 +4,10 @@ class SessionActivityController < ApplicationController
 
   def destroy
     redirect_address = new_user_session_path
-    redirect_address = IdpSettingsAdapter.get_idp_homepage(session[:saml_issuer]) if session[:saml_issuer]
+    if session[:saml_issuer]
+      redirect_address = IdpSettingsAdapter.get_idp_homepage(session[:saml_issuer])
+      session[:saml_issuer] = nil
+    end
     sign_out current_user
     respond_to do |format|
       format.js { render partial: 'destroy', locals: {redirect_address: redirect_address} }
